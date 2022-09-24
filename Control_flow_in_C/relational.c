a09bd04f5c9f3a1c0ae172ce72ad9322aef74990
mdNaimulHoque <naimulhoq89@gmail.com>
2022-09-24 20:48:42 +0600

    done

---
+++ Control_flow_in_C/relational.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 8, b = 8;
+
+    if (a < b)
+    {
+        printf("A < B");
+    }
+
+    else if (a <= b)
+    {
+        printf("A < B");
+    }
+
+    return 0;
+}
